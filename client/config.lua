68aec3360f2219a67b87223f13867e8776f598c0
ItzGoatATL <108423328+ItzGoatATL@users.noreply.github.com>
2024-10-15 10:25:27 -0400

    turned off test commands

--- client/config.lua
+++ client/config.lua
@@ -4,4 +4,4 @@
 
 config = config or {}
 
-config.use_test_commands = true -- Enable/disable test commands here; true = enabled, false = disabled
+config.use_test_commands = faalse -- Enable/disable test commands here; true = enabled, false = disabled
